495ed0cd01ab4cdb64a2d3764fb83de1f920c175
call_me_wolv <preethamdpg@gmail.com>
2019-08-05 04:13:02 +0530

    initial

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import quandl
+
+df = quandl.get('WIKI/GOOGL')
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High']-df['Adj. Close'])/df['Adj. Close']
+df['PCT_change'] = (df['Adj. Close']-df['Adj. Open'])/df['Adj. Open']
+df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
+print(df[:4])
+
